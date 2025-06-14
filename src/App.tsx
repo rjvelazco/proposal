9c2ca0be311f050ca0be7cc8b59800c677839467
Rafael Velazco <rjvelazco21@gmail.com>
2025-06-14 15:23:09 -0400

    Remove unnecessary React imports from App.tsx and CloudsBackground.tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ValentineProposal from './components/ValentineProposal'
 
 function App() {
--- src/components/CloudsBackground.tsx
+++ src/components/CloudsBackground.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './CloudsBackground.css';
 
 const clouds = [
